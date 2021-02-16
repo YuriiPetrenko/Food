757de18aa8157023f9f3c0b36b03464bd5cccaa5
yuriipetrenko <obzora6@gmail.com>
2021-02-16 05:16:46 +0100

    add commonjs moduls and webpack.config

---
+++ js/modules/cards.js
@@ -0,0 +1,49 @@
+function cards(){
+    /* Use (ES6) classes in real work */
+    class MenuCard {
+         constructor(src, alt, title, descr, price, parentSelector) {
+             this.src = src;
+             this.alt = alt;
+             this.title = title;
+             this.descr = descr;
+             this.price = price;
+             this.parent = document.querySelector(parentSelector);
+         }
+ 
+         render() {
+             const element = document.createElement('div');
+             element.innerHTML = `
+                 <div class="menu__item">
+                     <img src=${this.src} alt=${this.alt}>
+                     <h3 class="menu__item-subtitle">${this.title}</h3>
+                     <div class="menu__item-descr">${this.descr}</div>
+                     <div class="menu__item-divider"></div>
+                     <div class="menu__item-price">
+                         <div class="menu__item-cost">Price:</div>
+                         <div class="menu__item-total"><span>${this.price}</span> USD/day</div>
+                     </div>
+                 </div>
+             `;
+             this.parent.append(element);
+         }
+     }
+     
+     const getResource = async (url) =>{
+         const res = await fetch(url);
+ 
+         if(!res.ok){
+             throw new Error(`Could not frtch ${url}, status: ${res.status}`);
+         }
+ 
+         return await res.json();
+     };
+ 
+     getResource('http://localhost:3000/menu')
+     .then(data => {
+         data.forEach(({img, altimg, title, descr, price}) => {
+             new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+         });
+     });
+}
+
+module.exports = cards;
